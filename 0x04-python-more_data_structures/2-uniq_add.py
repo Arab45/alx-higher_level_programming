4db4290764c4e23b4966b9737ddb3d3142ec8444
Arab45 <horlakunleolayiwola@gmail.com>
2022-12-09 02:49:50 +0000

    more data strc

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list (only once for each integer)."""
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
